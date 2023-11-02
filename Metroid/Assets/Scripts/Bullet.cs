18918f1328cf1906e1b485cfeac7127aab02d76f
J2ExSlayer <jarroyo1@csuchico.edu>
2023-11-02 13:04:07 -0700

    Gun updates as of 11-2-2023 1pm

--- Metroid/Assets/Scripts/Bullet.cs
+++ Metroid/Assets/Scripts/Bullet.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    public float speed;
+    public bool goingRight;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+            Destroy(this.gameObject);
+        
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,16 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (goingRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
     }
+    
+
+
 }
--- Metroid/Assets/Scripts/Gun.cs
+++ Metroid/Assets/Scripts/Gun.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+
+    public GameObject bulletPrefab;
+
+    public float spawnrate = 1f;
+
+    public bool shootRight = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("ShootBullet", 0, spawnrate);
     }
 
     // Update is called once per frame
@@ -15,4 +23,12 @@ public class Gun : MonoBehaviour
     {
         
     }
+
+    private void ShootBullet()
+    {
+        GameObject bulletInstance = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        bulletInstance.GetComponent<Bullet>().goingRight = shootRight;
+    }
+
+
 }
